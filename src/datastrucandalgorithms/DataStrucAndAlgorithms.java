1453c8d2b977408125e2a17a10a1f26d75ca06c1
Brian Jerardi <bjerardi@gmail.com>
2012-05-10 20:31:10 -0400

    Initial Commit

---
+++ src/datastrucandalgorithms/DataStrucAndAlgorithms.java
@@ -0,0 +1,18 @@
+package datastrucandalgorithms;
+
+/**
+ * A command line menu to run sample programs
+ * @author Brian Jerardi
+ * @date 05/10/2012
+ */
+public class DataStrucAndAlgorithms 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+}
